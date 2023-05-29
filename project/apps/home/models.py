f17db3c22382bda12b6e846769c37cad22441c6e
Tomas Figoli <tfigoli1@gmail.com>
2023-05-29 19:17:58 -0300

    creados models

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=50)
+    cuit=models.IntegerField(max_length=11, unique=True)
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.cuit}"
+
+class Productos(models.Model):
+    producto=models.CharField(max_length=50, unique=True)
+    descripcion=models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return f"{self.producto}: {self.descripcion}"
+
+class Vendedores(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
